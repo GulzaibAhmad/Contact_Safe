40fa8e3aba1e510c4289c2d6e1b0df3574a5caaf
Gulzaib Ahmad <128983258+GulzaibAhmad@users.noreply.github.com>
2023-08-24 13:18:06 +0500

    server deleted

--- server/config/dbConnection.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg');
-
-const connectionString = process.env.CONNECTION_STRING;
-
-const client = new Client({
-  connectionString: connectionString,
-});
-
-module.exports = client;
