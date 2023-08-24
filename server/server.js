693ecf289be8a6ad0481096ea48e26c3983a4a89
Gulzaib Ahmad <128983258+GulzaibAhmad@users.noreply.github.com>
2023-08-24 13:41:58 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8081;
 client.connect();
 app.use(express.json());
 
-app.use(cors());
+app.use(cors("*"));
 
 app.use('/api/contacts', require('./routes/contactRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
